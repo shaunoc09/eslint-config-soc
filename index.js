a57d439dcc24d42c46db5c0bcd1728932f07c910
Shaun O'Connor <Shaun.OConnor@fleetmatics.com>
2017-03-03 13:53:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+    rules: {
+        "no-console" : "off",
+        "indent" : [
+            "error",
+            2
+        ],
+        "linebreak-style" : [
+            "error",
+            "unix"
+        ],
+        "quotes" : [
+            "error",
+            "single"
+        ],
+        "semi" : [
+            "error",
+            "always"
+        ],
+
+    }
+}
